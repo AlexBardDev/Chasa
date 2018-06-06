23fc6f051bffbeffeff527a3d6e63cb3a08a26ed
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-06-06 11:15:27 +0200

    Home, Events and About pages added with a carousel and the navigation

--- web_app/views.py
+++ web_app/views.py
@@ -6,3 +6,13 @@ def home(request):
     """This view returns the home page."""
 
     return render(request, "web_app/home.html")
+
+def events(request):
+    """This view returns a calendar with the events of the diving club."""
+
+    return render(request, "web_app/events.html")
+
+def about(request):
+    """This view returns the 'about us' web page."""
+
+    return render(request, "web_app/about.html")
