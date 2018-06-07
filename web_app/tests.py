5103eaa8ae88da4671081ed169940354bece9947
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-06-07 11:48:15 +0200

    Tests added and the website is ready for pre-production

--- web_app/tests.py
+++ web_app/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ViewsTests(TestCase):
+    """This class contains all the tests for the views of this app."""
+
+    def test_home(self):
+        """This method tests the 'home' view."""
+
+        response = self.client.get(reverse('home'))
+
+        assert response.status_code == 200
+
+    def test_events(self):
+        """This method tests the 'events' view."""
+
+        response = self.client.get(reverse('events'))
+
+        assert response.status_code == 200
+
+    def test_about(self):
+        """This method tests the 'about' view."""
+
+        response = self.client.get(reverse('about'))
+
+        assert response.status_code == 200
