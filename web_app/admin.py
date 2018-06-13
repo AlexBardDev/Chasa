193cfafa65c0a39844fca55e94a2b8f533e68eb6
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-06-13 14:56:41 +0200

    New feature added : we can save the diving events and the admin interface is customized for that

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Places, PersonInChargeOf, DivingEvents
 
 # Register your models here.
+
+class PlacesAdmin(admin.ModelAdmin):
+    """This class customizes the admin interface for the 'Places' table."""
+
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Places, PlacesAdmin)
+
+class PersonInChargeOfAdmin(admin.ModelAdmin):
+    """This class customizes the admin interface for the 'PersonInChargeOf'
+    table."""
+    
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(PersonInChargeOf, PersonInChargeOfAdmin)
+
+class DivingEventsAdmin(admin.ModelAdmin):
+    """This class customizes the admin interface for the 'DivingEvents' 
+    table."""
+
+    list_display = ('date', 'place', 'person', 'extra_infos',)
+    list_filter = ('place', 'person',)
+    ordering = ('date',)
+    search_fields = ('extra_infos',)
+
+admin.site.register(DivingEvents, DivingEventsAdmin)
--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Places(models.Model):
+    """This class contains all about the places."""
+
+    name = models.CharField(max_length=50, verbose_name="Site de plongée")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Lieu"
+        verbose_name_plural = "Lieux"
+
+class PersonInChargeOf(models.Model):
+    """This class contains all the people who can be in charge of a event."""
+
+    name = models.CharField(max_length=50, verbose_name="Personne en charge")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Responsable"
+        verbose_name_plural = "Responsables"
+
+class DivingEvents(models.Model):
+    """This class contains all the data about the diving events."""
+
+    date = models.DateTimeField(verbose_name="Date et heure d'arrivée")
+    place = models.ForeignKey(Places, on_delete=models.CASCADE)
+    person = models.ForeignKey(PersonInChargeOf, on_delete=models.CASCADE)
+    extra_infos = models.TextField(verbose_name="Infos supp", null=True, blank=True)
+
+    def __str__(self):
+        return """Plongée à {} le {}.""".format(self.place.name, self.date)
+
+    class Meta:
+        verbose_name = "Plongée"
+        verbose_name_plural = "Plongées"
+
+class ClubEvents(models.Model):
+    """This class contains all the data about the club events.
+    It's empty for the moment."""
+
+    pass
